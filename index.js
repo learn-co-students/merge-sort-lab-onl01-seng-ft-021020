c792c42ae3d24e245a6c49f12064be14bcba1bc6
lorrydriveloper <lorrydriveloper@gmail.com>
2020-08-21 11:32:24 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+function findMinAndRemoveSorted(array) {
+  return array.shift();
+}
 
+function merge(leftArr, rightArr) {
+  let sorted = [];
+
+  while (leftArr.length && rightArr.length) {
+    if (leftArr[0] <= rightArr[0]) {
+      sorted.push(findMinAndRemoveSorted(leftArr));
+    } else {
+      sorted.push(findMinAndRemoveSorted(rightArr));
+    }
+  }
+  return [...sorted, ...leftArr, ...rightArr];
+}
+
+function mergeSort(array) {
+  if (array.length < 2) {
+    return array;
+  }
+  let midpoint = Math.floor(array.length / 2);
+  let leftArr = array.slice(0, midpoint);
+  let rightArr = array.slice(midpoint);
+
+  return merge(mergeSort(leftArr), mergeSort(rightArr));
+}
